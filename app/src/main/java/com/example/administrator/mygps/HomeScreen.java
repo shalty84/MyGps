9937c8ad89764c0d3b7bc5231735e8afeec5ec0a
Shealtiel <shealtiel84@gmail.com>
2017-06-04 13:42:44 +0300

    shalty

--- app/src/main/java/com/example/administrator/mygps/HomeScreen.java
+++ app/src/main/java/com/example/administrator/mygps/HomeScreen.java
@@ -43,7 +43,7 @@ public class HomeScreen extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //shalty
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
